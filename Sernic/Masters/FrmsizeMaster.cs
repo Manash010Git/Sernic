d9cb951826b3c87aaddd81c94fea37eb47c69b4b
RUCHITA <RUCHITA@DESKTOP-8GAARUG>
2023-07-03 00:11:44 +0530

    add size master

---
+++ Sernic/Masters/FrmsizeMaster.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sernic.Masters
+{
+    public partial class FrmsizeMaster : Form
+    {
+        public FrmsizeMaster()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnEdit_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
